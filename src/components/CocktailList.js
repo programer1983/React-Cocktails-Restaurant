c8c5d25ccfe47e94dcea5dd9be98e46e0e181c85
programer1983 <programer972@gmail.com>
2022-03-21 15:08:30 +0200

    commit-5;

---
+++ src/components/CocktailList.js
@@ -0,0 +1,8 @@
+
+const CocktailList = () => {
+  return (
+    <h1>CocktailList</h1>
+  )
+}
+
+export default CocktailList
---
+++ src/components/SearchForm.js
@@ -0,0 +1,9 @@
+
+
+const SearchForm = () => {
+  return (
+    <h1>SearchForm</h1>
+  )
+}
+
+export default SearchForm
