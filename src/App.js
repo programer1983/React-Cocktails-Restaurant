2b3196b9259a4fc184171f707d2128e38ae9a968
programer1983 <programer972@gmail.com>
2022-03-22 09:58:54 +0200

    commit-9;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SingleCocktail from "./pages/SingleCocktail"
+import Error from "./pages/Error"
 
 function App() {
   return (
-      <h2>Hello World</h2>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cocktail/:id" element={<SingleCocktail />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router> 
   );
 }
 
