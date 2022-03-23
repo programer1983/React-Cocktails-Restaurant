b1fed9e1d20b48bf21fab3a05ad83e13a32182fc
programer1983 <programer972@gmail.com>
2022-03-23 10:05:27 +0200

    commit-12;

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
 
 const About = () => {
   return (
-    <h2>About</h2>
+    <section className="section about-section">
+      <hi className="section-title">about us</hi>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius urna justo, id commodo turpis molestie non. Donec tempus est sed diam iaculis rutrum. Fusce viverra leo a arcu porta vestibulum. Integer interdum, justo in mollis cursus, augue dolor tristique diam, at finibus orci nisl id lorem. Sed rhoncus erat ac elementum finibus. Duis non vehicula risus. Morbi non eros dolor. Pellentesque porta commodo justo non feugiat. Donec molestie a lorem nec ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec vel feugiat nisl, ut vehicula orci. In hac habitasse platea dictumst.
+      </p>
+    </section>
   )
 }
 
