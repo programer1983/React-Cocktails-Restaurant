ad62117815014acdb72640076499f0544a625504
programer1983 <programer972@gmail.com>
2022-03-24 16:17:16 +0200

    commit-17;

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,7 +1,24 @@
+import {Link} from "react-router-dom"
 
-const Cocktail = () => {
+
+const Cocktail = ({image, id, name, info, glass}) => {
   return (
-    <h2>Cocktail</h2>
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div className="cocktail-footer">
+        <h3>{name}</h3>
+        <h4>{glass}</h4>
+        <p>{info}</p>
+        <Link 
+           to={`/cocktail/${id}`} 
+           className="btn btn-primary btn-details"
+          >
+           details
+        </Link>
+      </div>
+    </article>
   )
 }
 
